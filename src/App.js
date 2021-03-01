320cbe84592024f202b14c76a45f8e77118346fa
jcamil0 <jcamilo.llanos@hotmail.com>
2021-03-01 14:46:38 -0500

    header work

--- src/App.js
+++ src/App.js
@@ -16,15 +16,14 @@ function App() {
           <Grid item xs={12} sm={12} md={4} lg={3} >
             <Profile />
           </Grid>
-          <Grid item xs style={{backgroundColor:'red'}}>
-            <Header />
 
+          <Grid item xs>
             <Router>
+            <Header />
               <Switch>
                 <Route path='/portfolio'>
                   <Portfolio />
                 </Route>
-
                 <Route path='/'>
                   <Resume />
                 </Route>
